a24cf8c1b015243a1c0e876cc5fa6cb6fd3ea5b9
Mattis Deisen <mattis@deisen.net>
2020-05-21 11:21:34 +0200

    Add Moving Average and Pct_Change Calculation
    Refactor pipeline

--- dataPrep/apply_pct_change.py
+++ dataPrep/apply_pct_change.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
+
 # Takes a dataframe, the column to groupby, and a list of columns
-# Normalizes them within each group
-def normalize_by_columns(df: pd.DataFrame, groupby: str, columns: list) -> pd.DataFrame:
+# Applies percent change to columns within each group
+def apply_pct_change(df: pd.DataFrame, groupby: str,
+                         columns: list) -> pd.DataFrame:
     ids = df[groupby].unique()
     output_df = pd.DataFrame()
 
     for current_id in ids:
         current = df.loc[df[groupby] == current_id]
-        current[columns] = current[columns].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+        current[columns] = current[columns].pct_change(fill_method='ffill')
         output_df = output_df.append(current)
     return output_df
-
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from team_project.dataPrep.apply_moving_average import apply_moving_average
+from team_project.dataPrep.apply_pct_change import apply_pct_change
 from team_project.dataPrep.collapse_dataframe_into_new import \
     collapse_dataframe_into_new
 from team_project.dataPrep.normalize_by_columns import normalize_by_columns
@@ -19,17 +21,32 @@ collapse_on = 'Deaths'
 # int
 threshhold = 20
 
+
 # Define the columns that should be normalized (after collapse)
 # list
 columns_to_normalize = ["Cases", "Deaths"]
 
+# Apply moving average window (will be applied to columns_to_normalize)
+# int
+window = 7
+
 # Choose the input file
 df = pd.read_csv('./2020-05-16_GoogleMobilityDataGlobal_joined.csv')
 
-# Collapse step
+# First collapse the data Collapse step
 df = collapse_dataframe_into_new(df, groupby, collapse_on, threshhold)
 df.to_csv('./collapse_on_' + collapse_on + '.csv')
 
 # Normalization step
-df = normalize_by_columns(df, groupby, columns_to_normalize)
-df.to_csv('./normalized_df.csv')
+# Only necessary if change in %_cases is not used
+# df = normalize_by_columns(df, groupby, columns_to_normalize)
+# df.to_csv('./normalized_df.csv')
+
+# Try Moving Average over period X
+df = apply_moving_average(df, groupby, columns_to_normalize, window)
+df.to_csv('./moving_average.csv')
+
+# Calculate %_change in target variable
+df = apply_pct_change(df, groupby, columns_to_normalize)
+df.to_csv('./pct_change.csv')
+
